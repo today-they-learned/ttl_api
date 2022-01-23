5b2b392bfea30386f5e66b4494b8229e8ffb5624
shinkeonkim <singun11@kookmin.ac.kr>
2022-01-23 16:58:12 +0900

    feat: Base API View 추가

---
+++ config/views.py
@@ -0,0 +1,28 @@
+import logging
+
+from django.views.generic.base import View
+
+
+class BaseView(View):
+    """BaseView"""
+
+    logger = logging.getLogger(__name__)
+    lookup_field = "id"
+
+    @property
+    def current_user(self):
+        """Return request user(current user)"""
+
+        return self.request.user
+
+    @property
+    def is_authenticated_user(self):
+        """Return whether current user is authenticated"""
+
+        return self.current_user.is_authenticated
+
+    @property
+    def is_anonymous_user(self):
+        """Return whether current user is anonymous"""
+
+        return self.current_user.is_anonymous
