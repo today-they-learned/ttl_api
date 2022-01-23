1defad07ed75f46209c7462ec44b3fbd95816fb3
shinkeonkim <singun11@kookmin.ac.kr>
2022-01-23 16:22:32 +0900

    feat: __init__.py에 account model들 추가

--- user/models/__init__.py
+++ user/models/__init__.py
@@ -1,1 +1,5 @@
 from .user import User
+from .sns_account import SnsAccount
+from .facebook_account import FacebookAccount
+from .instagram_account import InstagramAccount
+from .twitter_account import TwitterAccount
