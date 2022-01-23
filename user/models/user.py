ad884b52d7fea15b781af5ab5213c7a29c9f6d33
shinkeonkim <singun11@kookmin.ac.kr>
2022-01-23 14:26:04 +0900

    feat: 논의 과정 중에 나온 user 모델 추가 사항

--- user/models/user.py
+++ user/models/user.py
@@ -43,24 +43,39 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name=_("email"),
         max_length=200,
         unique=True,
-    )
+    )  # 이메일
     username = models.CharField(
         verbose_name=_("username"),
         max_length=50,
         unique=True,
-    )
+    )  # 유저네임
     is_active = models.BooleanField(
         verbose_name=_("Is active"),
         default=True,
-    )
+    )  # 유저가 활성화되어있는지 여부
+    introduce = models.TextField(
+        verbose_name=_("introducing content"),
+        null=True,
+        blank=True,
+    )  # 유저의 소개말
+    repository = models.TextField(
+        verbose_name=_("refreshing github repository"),
+        null=True,
+        blank=True,
+    )  # 유저의 TIL Github Repository, username/repo_name 형식으로 담아져야 합니다.
+    subscribe_recommended_mail = models.BooleanField(
+        verbose_name=_("whether subscribe recommended mail"),
+        default=False,
+        null=False,
+    )  # 추천 메일을 구독했는지 여부를 나타냅니다.
     created_at = models.DateTimeField(
         verbose_name=_("created at"),
         auto_now_add=True,
-    )
+    )  # 유저 레코드가 생성된 일저
     updated_at = models.DateTimeField(
         verbose_name=_("updated at"),
         auto_now=True,
-    )
+    )  # 유저 레코드가 수정된 일자
 
     objects = UserManager()
 
