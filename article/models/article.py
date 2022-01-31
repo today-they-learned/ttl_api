00c9409d9efba7e2f49ed3b5adb68a75b2a191a3
shinkeonkim <singun11@kookmin.ac.kr>
2022-02-01 02:14:20 +0900

    chore: beautify article model

--- article/models/article.py
+++ article/models/article.py
@@ -3,41 +3,51 @@ from user.models import User
 
 
 SOURCE_CHOICES = [
-    ('tl','TTL'),
-    ('vl','Velog'),
-    ('gh','Github'),
-    ('ts','Tistory')
+    ("tl", "TTL"),
+    ("vl", "Velog"),
+    ("gh", "Github"),
+    ("ts", "Tistory"),
 ]
+
 
 class Article(models.Model):
     """Model definition for Article."""
-    
+
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name = "articles",
-        verbose_name="writer"
+        related_name="articles",
+        verbose_name="writer",
     )
     title = models.CharField(
-        max_length=50
+        max_length=50,
     )
-    content = models.TextField()
+    content = models.TextField(
+        null=True,
+        blank=True,
+    )
     source = models.CharField(
         null=True,
         choices=SOURCE_CHOICES,
-        default='tl',
-        max_length=2)
+        default="tl",
+        max_length=2,
+    )
     thumbnail = models.ImageField(
         upload_to="article/",
-        blank = True,
-        null=True
+        blank=True,
+        null=True,
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
     def __str__(self):
         return self.title
+
     class Meta:
-        verbose_name="Article"
+        verbose_name = "Article"
         verbose_name_plural = "Articles"
-        db_table="articles"
+        db_table = "articles"
