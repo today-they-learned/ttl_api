03d4c09f8c820e4f3ab9e9e48aed18519706cf89
shinkeonkim <singun11@kookmin.ac.kr>
2022-01-31 15:38:04 +0900

    feat: staticfiles settings

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from typing import List
 import environ
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -155,8 +156,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = "static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
