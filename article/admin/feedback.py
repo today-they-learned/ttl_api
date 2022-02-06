1edd7f148732f491eefe7299923c13db8939d62d
seunghee jin <sualng123@gmail.com>
2022-02-06 10:32:52 +0900

    feedback 모델,admin

---
+++ article/admin/feedback.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from article.models import Feedback
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    """Admin View for Feedback"""
+
+    list_display = (
+        "user",
+        "article",
+        "created_at",
+    )
+    readonly_fields = (
+        "created_at",
+        "user",
+        "article",
+    )
+    ordering = (
+        "created_at",
+    )
--- article/models/__init__.py
+++ article/models/__init__.py
@@ -1,2 +1,3 @@
 from .article import Article
 from .bookmark import Bookmark
+from .feedback import Feedback
