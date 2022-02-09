067c0b11a87cd7651644645019b4869e4261e4a7
Jihyun <034179@naver.com>
2022-02-10 02:40:29 +0900

    feat: user 공개 프로필 API

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
+from user.views import UserRetrieveAPIView
+
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
     path("", include("dj_rest_auth.registration.urls")),
     path("api-token-auth/", obtain_auth_token),
+    path("<int:id>", UserRetrieveAPIView.as_view()),
 ]
