74be3c5e69f198f91857e71d9f1659b6e5ddd77f
shinkeonkim <singun11@kookmin.ac.kr>
2022-02-12 21:10:21 +0900

    Revert "Revert "Revert "chore: dj_rest_auth_view custom"""
    This reverts commit aabda57f9be629a93de01e02007683f5a4db09c5.

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from django.conf import settings
-
 
 from user.views import (
     FollowCreateAPIView,
@@ -9,42 +7,15 @@ from user.views import (
     FollowingListView,
     FollowerListView,
     UserRetrieveAPIView,
-    PasswordResetConfirmView,
-    PasswordChangeView,
-    PasswordResetView,
-    LoginView,
-    LogoutView,
-    UserDetailsView,
 )
 
 urlpatterns = [
+    path("", include("dj_rest_auth.urls")),
+    path("", include("dj_rest_auth.registration.urls")),
+    path("api-token-auth/", obtain_auth_token),
     path("<int:id>/", UserRetrieveAPIView.as_view()),
     path("<int:id>/follow", FollowCreateAPIView.as_view()),
     path("<int:id>/unfollow", FollowDestroyAPIView.as_view()),
     path("<int:id>/follwing", FollowingListView.as_view()),
     path("<int:id>/follwer", FollowerListView.as_view()),
-    path("password/reset/", PasswordResetView.as_view(), name="rest_password_reset"),
-    path(
-        "password/reset/confirm/",
-        PasswordResetConfirmView.as_view(),
-        name="rest_password_reset_confirm",
-    ),
-    path("login/", LoginView.as_view(), name="rest_login"),
-    # URLs that require a user to be logged in with a valid session / token.
-    path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
-    path("password/change/", PasswordChangeView.as_view(), name="rest_password_change"),
-    path("", include("dj_rest_auth.registration.urls")),
-    path("api-token-auth/", obtain_auth_token),
 ]
-
-
-if getattr(settings, "REST_USE_JWT", False):
-    from rest_framework_simplejwt.views import TokenVerifyView
-
-    from dj_rest_auth.jwt_auth import get_refresh_view
-
-    urlpatterns += [
-        path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-        path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
-    ]
--- user/views/__init__.py
+++ user/views/__init__.py
@@ -1,12 +1,5 @@
+#from .facebook_account_update_api_view import FacebookAccountUpdateAPIView
 from .follow_create_api_view import FollowCreateAPIView
 from .follow_destroy_api_view import FollowDestroyAPIView
 from .follow_list_api_view import FollowingListView, FollowerListView
 from .user_retrieve_api_view import UserRetrieveAPIView
-from .dj_rest_auth_view import (
-    LoginView,
-    LogoutView,
-    PasswordResetView,
-    PasswordChangeView,
-    PasswordResetConfirmView,
-    UserDetailsView,
-)
