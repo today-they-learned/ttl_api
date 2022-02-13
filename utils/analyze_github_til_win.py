69e6baa68346c8526823a360d1b190eaf90f395c
Jihyun <034179@naver.com>
2022-02-14 01:48:04 +0900

    feat: github 추합기 생성일자, 수정일자 추가

--- utils/analyze_github_til_win.py
+++ utils/analyze_github_til_win.py
@@ -1,15 +1,18 @@
 from git import Repo
+from pydriller import Repository
 import os
 import shutil
 import aiofiles
 import asyncio
 
+"""github_collector window version"""
 
 class AnalyzeGithubTil:
     def __init__(self, username, repository):
         self.username = username
         self.repository = repository
         self.results = []
+        self.date = {}
 
     def clone_repositry(self):
         """Repository를 특정 디렉토리에 Clone 합니다."""
@@ -41,9 +44,28 @@ class AnalyzeGithubTil:
 
         tasks = set()
         for path, file_name in path_list:
+            if file_name == "README.md" or file_name == "readme.md":    # readme 파일을 제외
+                continue
             tasks.add(self.get_file_content(path, file_name))
 
         asyncio.run(asyncio.wait(tasks))
+
+    def get_date(self):
+        """파일의 생성일자, 수정일자 딕셔너리"""
+        for commit in Repository(f'https://github.com/{self.username}/{self.repository}').traverse_commits():
+            file_date = str(commit.committer_date)
+
+            for file in commit.modified_files:
+                file_path = file.new_path or file.old_path
+
+                if file.filename == "README.md" or file.filename == "readme.md":  # readme 파일을 제외
+                    continue
+
+                if file_path in self.date:
+                    self.date[file_path].append(file_date)
+                    continue
+
+                self.date[file_path] = [file_date]
 
     def __del__(self):
         """작업을 마치면, 클론한 디렉토리를 제거합니다."""
@@ -51,6 +73,8 @@ class AnalyzeGithubTil:
 
 
 if __name__ == "__main__":
-    t = AnalyzeGithubTil("shinkeonkim", "TIL")
+    t = AnalyzeGithubTil("Jihyun-Choi", "TIL_test")
     t.perform()
+    t.get_date()
+    print(t.date)
     print(t.results)
