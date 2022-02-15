c06ef5e32b674a9e8399277d2ab86b67d9490671
shinkeonkim <singun11@kookmin.ac.kr>
2022-02-15 22:38:16 +0900

    chore: article list serializer 분리

---
+++ article/serializers/article_list_serializer.py
@@ -0,0 +1,57 @@
+from django.db.models import Count
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from rest_framework import serializers
+
+from tag.serializers import TagListSerializerField, TaggitSerializer
+from article.models import Article, Feedback
+from user.serializers import UserSerializer
+
+
+class ArticleListSerializer(TaggitSerializer, WritableNestedModelSerializer):
+    """Serializer definition for Article Model."""
+
+    tags = TagListSerializerField(
+        required=False,
+        read_only=False,
+    )
+    user = UserSerializer(
+        read_only=True,
+        required=False,
+    )
+    feedback = serializers.SerializerMethodField()
+
+    class Meta:
+        """Meta definition for ArticleSerializer."""
+
+        model = Article
+        fields = [
+            "id",
+            "user",
+            "title",
+            "content",
+            "tags",
+            "thumbnail",
+            "study_count",
+            "feedback_count",
+            "feedback",
+            "created_at",
+            "updated_at",
+        ]
+
+        read_only_fields = [
+            "id",
+            "user",
+            "study_count",
+            "feedback_count",
+            "feedback",
+            "created_at",
+            "updated_at",
+        ]
+
+    def get_feedback(self, obj):
+        return (
+            Feedback.objects.filter(article=obj)
+            .values("category")
+            .annotate(total=Count("category"))
+            .order_by("-total")
+        )
--- comment/serializers/comment_serializer.py
+++ comment/serializers/comment_serializer.py
@@ -2,9 +2,16 @@ from rest_framework.serializers import ModelSerializer
 
 from comment.models import Comment
 
+from user.serializers import UserSerializer
+
 
 class CommentSerializer(ModelSerializer):
     """Serializer definition for Comment Model."""
+
+    user = UserSerializer(
+        read_only=True,
+        required=False,
+    )
 
     class Meta:
         """Meta definition for CommentSerializer."""
