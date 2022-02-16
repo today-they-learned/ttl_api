72673558da5dfb44c824af3ba17a179f8c6f212e
shinkeonkim <singun11@kookmin.ac.kr>
2022-02-17 00:19:56 +0900

    fix: urls

--- article/urls.py
+++ article/urls.py
@@ -15,9 +15,9 @@ app_name = "article"
 urlpatterns = [
     path("", ArticleListCreateAPIView.as_view()),
     path("<int:id>/", ArticleRetrieveUpdateDestroyAPIView.as_view()),
-    path("<int:article_id>/bookmark", BookmarkCreateAPIView.as_view()),
-    path("<int:article_id>/unbookmark", BookmarkDestroyAPIView.as_view()),
-    path("<int:article_id>/feedback/<category>", FeedbackCreateAPIView.as_view()),
-    path("<int:article_id>/feedback", FeedbackDestroyAPIView.as_view()),
+    path("<int:article_id>/bookmark/", BookmarkCreateAPIView.as_view()),
+    path("<int:article_id>/unbookmark/", BookmarkDestroyAPIView.as_view()),
+    path("<int:article_id>/feedback/<category>/", FeedbackCreateAPIView.as_view()),
+    path("<int:article_id>/feedback/", FeedbackDestroyAPIView.as_view()),
     path("upload_image/", UploadImageCreateAPIView.as_view()),
 ]
