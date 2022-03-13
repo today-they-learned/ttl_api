6f537045f7baa75e2db12063904d4ffe3be8e5ec
seunghee jin <sualng123@gmail.com>
2022-03-13 14:43:30 +0900

    fix: tab=study 인 경우 articles 를 조회순으로 정렬

--- article/views/article_list_create_api_view.py
+++ article/views/article_list_create_api_view.py
@@ -59,8 +59,8 @@ class ArticleListCreateAPIView(BaseView, ListCreateAPIView):
                 studied_article_ids = Study.objects.filter(
                     user=self.current_user
                 ).values_list("article__id", flat=True)
-
-                queryset = queryset.filter(id__in=studied_article_ids)
+                
+                queryset = queryset.filter(id__in=studied_article_ids).order_by('studies')
 
         if user_id is not None:
             queryset = queryset.filter(user__id=user_id)
