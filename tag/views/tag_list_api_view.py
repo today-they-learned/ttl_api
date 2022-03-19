9fdd575c5db0cc686ad341a03284e629e947c66b
shinkeonkim <singun11@kookmin.ac.kr>
2022-03-19 10:36:32 +0900

    feat: 태그 API를 비로그인 유저도 호출할 수 있도록 함.

--- tag/views/tag_list_api_view.py
+++ tag/views/tag_list_api_view.py
@@ -1,5 +1,7 @@
-from rest_framework.generics import ListAPIView
 from rest_framework import filters
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from config.views import BaseView
 from taggit.models import Tag
 from tag.serializers import TagSerializer
@@ -7,6 +9,7 @@ from tag.serializers import TagSerializer
 
 class TagListAPIView(BaseView, ListAPIView):
     serializer_class = TagSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Tag.objects.all()
     filter_backends = [filters.SearchFilter]
     search_fields = ["^name"]
